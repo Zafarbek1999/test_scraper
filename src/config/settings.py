28b0db03990061f63a31d327e21222b12de1a3e1
Zafarbek Kamuljonov <zafarbek.kamuljonov@sourcefit.net>
2025-08-18 14:34:05 +0500

    changed default page size

--- src/config/settings.py
+++ src/config/settings.py
@@ -155,7 +155,7 @@ CACHES = {
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ['rest_framework.filters.SearchFilter'],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 100
+    'PAGE_SIZE': 5
 }
 
 REMOTEOK_BASE_URL = "https://remoteok.com"
